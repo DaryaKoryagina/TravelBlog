b5ab7a7937d1b87c09c7ed088ad5df31a389685d
DaryaKoryagina <koryagina.darina@yandex.ru>
2020-08-19 17:29:36 +0300

    slider

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,22 @@ import FlickityFull from 'flickity-fullscreen'
 import FlickityNav from 'flickity-as-nav-for'
 import * as select2 from 'select2'
 console.log(67676)
+
+let carouselMain = document.querySelector('.slider-main')
+console.log(carouselMain)
+if (carouselMain) {
+    let flkty = new FlickityFull(carouselMain, {
+        prevNextButtons: true,
+        pageDots: false,
+        fullscreen: false,
+    });
+    // var previousButton = document.querySelector('#start__btn');
+    // previousButton.addEventListener('click', function() {
+    //     flkty.previous();
+    // });
+
+    // var nextButton = document.querySelector('#end__btn');
+    // nextButton.addEventListener('click', function() {
+    //     flkty.next();
+    // });
+};
