2ad47ce9b74f283bb0daecc3b8fd18140e17d73d
DaryaKoryagina <koryagina.darina@yandex.ru>
2020-08-22 02:05:00 +0300

    header ,slider

--- src/js/app.js
+++ src/js/app.js
@@ -2,26 +2,27 @@ import * as $ from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
 import Flickity from 'flickity'
-import FlickityFull from 'flickity-fullscreen'
-import FlickityNav from 'flickity-as-nav-for'
-import * as select2 from 'select2'
-console.log(67676)
 
-let carouselMain = document.querySelector('.slider-main')
-console.log(carouselMain)
-if (carouselMain) {
-    let flkty = new FlickityFull(carouselMain, {
-        prevNextButtons: true,
-        pageDots: false,
-        fullscreen: false,
-    });
-    // var previousButton = document.querySelector('#start__btn');
-    // previousButton.addEventListener('click', function() {
-    //     flkty.previous();
-    // });
 
-    // var nextButton = document.querySelector('#end__btn');
-    // nextButton.addEventListener('click', function() {
-    //     flkty.next();
-    // });
-};
+$(document).ready(function($) {
+
+    let carouselMain = document.querySelector('.slider-main')
+    console.log(carouselMain)
+    if (carouselMain) {
+
+        let flkty = new Flickity(carouselMain, {
+            prevNextButtons: true,
+            pageDots: false
+        });
+        flkty.resize()
+            // var previousButton = document.querySelector('#start__btn');
+            // previousButton.addEventListener('click', function() {
+            //     flkty.previous();
+            // });
+
+        // var nextButton = document.querySelector('#end__btn');
+        // nextButton.addEventListener('click', function() {
+        //     flkty.next();
+        // });
+    };
+})
